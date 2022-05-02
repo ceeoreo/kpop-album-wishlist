94fdaa397d66f427094dc5edb958f80ddb565d28
Ceora Ford Work <ceorafordwork@MacBook-Pro-3.local>
2022-05-02 02:14:57 -0400

    add ability to add new cards but not with actual user input

--- js/index.js
+++ js/index.js
@@ -2,8 +2,14 @@ const addAnAlbumBtn = document.querySelector('.add-album-button');
 const wishModalBg = document.querySelector('.wish-modal-bg');
 const wishModal = document.querySelector('.wish-modal');
 // wishModal is the var for the form pop up
-const saveAlbum = document.getElementById('add-button');
+const saveAlbumBtn = document.getElementById('add-button');
 const cancelButton = document.getElementById('cancel');
+
+const albumCardsDisplay = document.querySelector('.album-cards')
+const albumName = document.getElementById('album');
+const artistName = document.getElementById('artist');
+const priceInput = document.getElementById('price');
+const linkInput = document.getElementById('link');
 
 //when cancel button is clicked, the wishModalBg is hidden again
 //when saveAlbum is clicked, information from the form is 
@@ -21,4 +27,38 @@ let cancelAdd = () => {
 }
 
 cancelButton.addEventListener('click', cancelAdd);
+
+//when add button is clicked, to save info,
+// create album card div which contains p for album 
+// artist price and list and add album form input to 
+// their respective p
+// this card will be displayed within album card display
+
+// Create album card
+let createAlbumCard = () => {
+    let albumCard = document.createElement('div');
+    albumCard.classList.add('album-card');
+    // let albumPar = document.createElement('p');
+    // let userAlbumAdd = albumName.innerText;
+
+    // albumPar.append(userAlbumAdd);
+    // albumCard.appendChild(albumPar);
+    albumCard.innerHTML= `<p> hello </p>`
+    return albumCard;
+}
+
+// Add album card to display
+
+let saveAlbumInfo = () => {
+    let albumCardToDisplay = createAlbumCard();
+    albumCardsDisplay.appendChild(albumCardToDisplay);
+    //append all those p's to the album card div
+    // let userArtistAdd = artistName.innerText;
+    // let userPriceAdd = priceInput.innerText;
+    // let userLinkAdd = linkInput.innerText;
+
+}
+
+saveAlbumBtn.addEventListener('click', saveAlbumInfo);
+saveAlbumBtn.addEventListener('click', cancelAdd);
 
