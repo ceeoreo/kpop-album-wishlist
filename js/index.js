ce72f7d3873e599b908310d600aa990436f15d4f
Ceora Ford Work <ceorafordwork@MacBook-Pro-3.local>
2022-05-01 22:10:55 -0400

    Testing album info cards

--- js/index.js
+++ js/index.js
@@ -5,18 +5,20 @@ const wishModal = document.querySelector('.wish-modal');
 const saveAlbum = document.getElementById('add-button');
 const cancelButton = document.getElementById('cancel');
 
-//when addAnAlbumBtn is clicked, wishModalBg css property visibility is no longer hidden
 //when cancel button is clicked, the wishModalBg is hidden again
 //when saveAlbum is clicked, information from the form is 
 
+//when add album is clicked, album form is no longer hidden
 let initiateAdd = () => {
     wishModalBg.classList.add('active-bg');
 }
 
 addAnAlbumBtn.addEventListener('click', initiateAdd);
 
+//when cancel button is clicked, album form is hidden again
 let cancelAdd = () => {
     wishModalBg.classList.remove('active-bg');
 }
 
 cancelButton.addEventListener('click', cancelAdd);
+
