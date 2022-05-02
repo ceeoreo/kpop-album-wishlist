9d52d1dccf347dafa2ebdad34db654cd1d581b27
Ceora Ford Work <ceorafordwork@MacBook-Pro-3.local>
2022-05-02 09:40:04 -0400

    Add checkbox for purchased? on album info cards

--- js/index.js
+++ js/index.js
@@ -61,55 +61,19 @@ let createAlbumCard = () => {
     pricePar.append(userPriceAdd);
     albumCard.appendChild(pricePar);
     
-    // let purchaseCheckBox = document.createElement('input');
-    // purchaseCheckBox.type = "checkbox";
-    // checkbox.name = "name";
-    // checkbox.value = "value";
-    // purchaseCheckBox.id = "purchase-checkbox";
-
     let purchaseLabel = document.createElement('label')
     purchaseLabel.htmlFor = "purchase-checkbox";
     purchaseLabel.appendChild(document.createTextNode('Purchased'));
     albumCard.appendChild(purchaseLabel);
-    // albumCard.appendChild(purchaseCheckBox);
+    
+    let purchaseCheckBox = document.createElement('input');
+    purchaseCheckBox.type = "checkbox";
+    purchaseCheckBox.id = "purchase-checkbox";
+    albumCard.appendChild(purchaseCheckBox);
+
     return albumCard;
 }
 
-// Function for adding prices
-let addAllPrices = () => {
-    let allPrices = document.querySelectorAll('.prices');
-    // let allPricesArr = Object.values(allPrices);
-
-    // console.log(allPricesArr);
-    // if (allPricesArr.length > 0) {
-    //     let numAllPrices = allPricesArr.map(priceText => Number(priceText.textContent));
-    //     numAllPrices.reduce((accumulator, currentValue) => {
-    //         accumulator + currentValue;
-    //         console.log(accumulator)
-    //         totalPriceDisplay.append(accumulator);
-    //         console.log('her')
-    //     }
-    // , 0);
-    // } else {
-    //     let price = allPricesArr[0];
-
-    //     let priceNum = Number(price.textContent)
-    //     console.log(priceNum);
-    //     totalPriceDisplay.append(priceNum);
-    // } 
-    for (let i=0; i <allPrices.length; i++){
-        let price = allPrices[i];
-
-        let priceNum = Number(price.textContent);
-
-        if (i === 0){
-            totalPriceDisplay.append(priceNum);
-        } else {
-            // let currentPrice = ;
-            console.log("hi");
-        }
-    }
-}
 
 // Add album card to display
 let saveAlbumInfo = (event) => {
